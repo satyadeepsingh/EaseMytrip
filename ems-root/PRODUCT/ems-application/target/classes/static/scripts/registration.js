d60c15130943b3315f35a75b9b79f67809a645e1
satyadeep.singh@gemalto.com <satyadeep.singh@gemalto.com>
2018-12-17 12:42:44 +0530

    redirect registration on success to login

--- ems-root/PRODUCT/ems-application/target/classes/static/scripts/registration.js
+++ ems-root/PRODUCT/ems-application/target/classes/static/scripts/registration.js
@@ -1,63 +1,92 @@
-
-var app = angular.module('myApp',[]);
+'use strict';
+var app = angular.module('myApp', []);
 console.log("registration.js loaded");
-app.factory('httpRequestInterceptor', function () {
-    return {
-      request: function (config) {
-        config.headers['Accept'] = 'application/json;odata=verbose';
-        config.headers['x-session-id'] = '355345';
-        return config;
-        }
-    };
+app.factory('httpRequestInterceptor', function() {
+	return {
+		request : function(config) {
+			config.headers['Accept'] = 'application/json;odata=verbose';
+			config.headers['x-session-id'] = Math.floor(100000000 + Math
+					.random() * 900000000);
+			;
+			return config;
+		}
+	};
 });
-  
-app.config(function ($httpProvider) {
-    $httpProvider.interceptors.push('httpRequestInterceptor');
-});
-app.controller('httpCtrl', function($scope, $http) {
 
-    $scope.firstName = null;
-    $scope.lastName = null;
-    $scope.useremail = null;
-    $scope.userpassword = null;
-    $scope.useraddress1 = null;
-    $scope.useraddress2 = null;
-    $scope.pincode = null;
-    $scope.city = null;
-    $scope.state = null;
-    $scope.country = null;
-    console.log("calling postRequest()");
-
-    
-    $scope.postRequest = function (firstName, lastName, useremail,userpassword,useraddress1,useraddress2,city,state, country,pincode) {
-        var createUserReq = {
-            name: $scope.firstName + " " + $scope.lastName,
-            email: $scope.useremail,
-            password: $scope.userpassword,
-            address : {
-                addressline1: $scope.useraddress1,
-                addressline2: $scope.useraddress2,
-                city:$scope.city,
-                country:$scope.country,
-                state: $scope.state,
-            },
-            pincode: $scope.pincode,
-            username: $scope.useremail
-        };
-        console.log($scope.userpassword);
-        console.log($scope.firstName);
-        //Call the services
-        $http.post('/emt/createuser', JSON.stringify(createUserReq))
-        .then(function (response) {
-            if (response.data) {
-                console.log(response.data);
-                $scope.msg = "Post Data Submitted Successfully!";
-            }
-        }, function (response) {
-            $scope.msg = "Service not Exists";
-            $scope.statusval = response.status;
-            $scope.statustext = response.statusText;
-            $scope.headers = response.headers();
-        });
-    };
+app.config(function($httpProvider) {
+	$httpProvider.interceptors.push('httpRequestInterceptor');
 });
+
+app
+		.controller(
+				'httpCtrl',
+				function($scope, $http, $window, $timeout) {
+
+					$scope.firstName = null;
+					$scope.lastName = null;
+					$scope.useremail = null;
+					$scope.userpassword = null;
+					$scope.useraddress1 = null;
+					$scope.useraddress2 = null;
+					$scope.pincode = null;
+					$scope.city = null;
+					$scope.state = null;
+					$scope.country = null;
+					$scope.contactnumber = null;
+					$scope.statusbody = null;
+					$scope.title = null;
+					$scope.status = null;
+					$scope.message = null;
+					
+					$scope.redirectToLogin = function() {
+						$window.location.href = "/login";
+					}
+
+					$scope.postRequest = function(firstName, lastName,
+							useremail, userpassword, useraddress1,
+							useraddress2, city, state, country, pincode,
+							contactnumber) {
+						var createUserReq = {
+							name : $scope.firstName + " " + $scope.lastName,
+							email : $scope.useremail,
+							password : $scope.userpassword,
+							address : {
+								addressline1 : $scope.useraddress1,
+								addressline2 : $scope.useraddress2,
+								city : $scope.city,
+								country : $scope.country,
+								state : $scope.state,
+							},
+							pincode : $scope.pincode,
+							username : $scope.useremail,
+							contactnumber : $scope.contactnumber
+						};
+
+						// Call the services
+						$http
+								.post('emt/createuser',
+										JSON.stringify(createUserReq))
+								.then(
+										function(response) {
+											if (response.data) {
+												$scope.statusbody = 'User Registration is successful. You are being redirected to login to continue';
+												$scope.title = 'Congrats, you are one step away from us!';
+												$scope.message = "success";
+												$timeout(function() {
+													$scope.redirectToLogin();
+												}, 1000);
+
+												// $window.locaton.href='/login';
+												console.log(response.data);
+											}
+										},
+										function(response) {
+											$scope.title = "Phshhhh, something wrong happened!";
+											$scope.statusbody = "User Registration has failed! Please check the details and try again.";
+											$scope.message="failure";
+											$scope.statusval = response.status;
+											$scope.statustext = response.statusText;
+											$scope.headers = response.headers();
+										});
+					};
+				});
